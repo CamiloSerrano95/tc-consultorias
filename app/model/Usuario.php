ccb840cf1f361c81b37fb439ad8e8173833db9d0
Camilo Serrano <casegica@gmail.com>
2019-10-11 07:52:57 -0500

    Model for users

--- app/model/Usuario.php
+++ app/model/Usuario.php
@@ -1,7 +1,18 @@
 <?php
     class UsuarioModel {
+        private $nombres;
+        private $user;
+        private $passwd;
+        private $rol;
+        private $DataBase;
+        private $Table = 'usuarios';
+
         public function __construct() {
-			$connection = new Conexion();
+            $this->nombres = "";
+            $this->user = "";
+            $this->passwd = "";
+            $this->rol = "";
+            $connection = new Conexion();
             $this->DataBase = $connection::get_conexion();
         }
     }
