db5096704eb766af6130111d4b68c57d33795dda
Camilo Serrano <casegica@gmail.com>
2019-10-11 09:57:16 -0500

    View for login

--- view/index.php
+++ view/index.php
@@ -5,12 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta http-equiv="refresh" content="0; url=view/Login/Login.php">
-    <title>Document</title>
+    <title>Login</title>
 </head>
-
 <body>
-
+  <h1>Aqui va el login, recuerda que es el index principal de la app</h1>
+  <a href="<?php echo ABS_PATH."usuario" ?>">Listar</a>
 </body>
 
 </html>
