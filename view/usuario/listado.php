3de15230f98e12465a5bf22d5a33bea9a5cf6170
Camilo Serrano <casegica@gmail.com>
2019-10-11 09:57:29 -0500

    Example redirect

---
+++ view/usuario/listado.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Listar usuarios</h1>
+    <a href="<?php echo ABS_PATH ?>">Inicio</a>
+</body>
+</html>
