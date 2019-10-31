cdeb833808c463bf1aa5ab3ffa30016c4aa38658
CamiloSerrano95 <casegica@gmail.com>
2019-10-31 16:38:44 -0500

    Documentation controller

--- app/Controllers/UsuarioController.php
+++ app/Controllers/UsuarioController.php
@@ -2,6 +2,18 @@
     use \vista\Vista;
 
     class UsuarioController {
+
+        // Los controladores deben ir estructurados con los siguientes 5 metodos
+        // index = es el metodo que renderiza el select from de una consulta sql
+        // show = es el metodo que renderiza la informacion de una consulta en especifico recibe como parametro la llave primaria
+        // create = es el metodo que renderiza la vista del formulario de guardado de la data recivida
+        // store = es el metodo que recibe y valida toda la informacion enviada por el formulario
+        // edit =  es el metodo que renderiza la vista de edicion de una data en particular, esta recibe la llave primaria como parametro
+        // update = es el metodo que recibe y valida toda la informacion enviada por el formulario para la actualizacion
+        // delete = este metodo recibe por parametro la llave primaria para eliminar un registro
+
+        // OJO para que esto pueda funcionar, cada controlador debe ser registrado en el archivo rutas.php 
+        //que se encuentra en la siguiente ruta app/rutas/rutas.php
         
         public function index() {
             return Vista::crear("inicio.index");
