cb31723bbb6c9a8b3a50404c970b7bbeb458b1d7
StivenCS <stivl52@hotmail.com>
2019-11-04 15:28:27 -0500

    :pig:

---
+++ app/rutas/rutas.php
@@ -0,0 +1,22 @@
+<?php
+//todas las rutas disponibles en nuestra aplicación
+$ruta = new Ruta();
+$ruta->controladores(array(
+    "/" => "WelcomeController",
+    "/login"    => "AuthController",
+    "/usuario"  => "UsuarioController",
+    "/service" => "ServiceController",
+    "/producto" => "ProductoController",
+    "/admin"    => "AdminController",
+    "/home"     => "HomeController",
+    "/busqueda" => "BusquedaPalabra",
+    "/code" => "controllercode",
+    "/empresa" => "controllercompany",
+    "/evaluacion" => "controllerevaluacion",
+    "/experiencia" => "controllerexperience",
+    "/cumplimientos" => "CumplimientosController",
+    "/vistaresults" => "ResultsController",  
+    "/revision" => "RevisionController",
+    "/Allempresas" => "AllEmpresasController",
+
+));
