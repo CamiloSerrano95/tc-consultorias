44901e1fb63b234b4ccc89521aed8806220b58fb
CamiloSerrano95 <casegica@gmail.com>
2019-11-09 18:28:30 -0500

    Ignored

---
+++ app/utils/Conexion.php
@@ -0,0 +1,21 @@
+<?php
+    class Conexion{	
+		public function get_conexion(){
+			$user = "root";
+			$pass = "";
+			$host = "localhost";
+            $db = "tconsultorias";
+            
+			try {
+				$connection = new PDO("mysql:host=$host; dbname=$db", $user, $pass);
+				$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            	$connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+				
+			} catch (Exception $e) {
+				print_r($e);
+				
+			}
+			return $connection;
+		}
+    }
+?>
